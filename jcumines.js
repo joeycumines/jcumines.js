7a641df2ad2adbca82bfb92003210a547f7bb285
Joseph Cumines <joeycumines@gmail.com>
2016-10-06 19:28:34 +1000

    js-beautified

--- jcumines.js
+++ jcumines.js
@@ -17,101 +17,101 @@ containing input's value.
 
 If no input is passed, then it will return the queued workers length.
  */
-jcumines.worker = function (key, input) {
-	//Initialize the worker if necessary
-	if (jcumines._workers[key] == null) {
-		jcumines._workers[key] = [];
-	}
+jcumines.worker = function(key, input) {
+    //Initialize the worker if necessary
+    if (jcumines._workers[key] == null) {
+        jcumines._workers[key] = [];
+    }
 
-	if (input == null)
-		return jcumines._workers[key].length;
+    if (input == null)
+        return jcumines._workers[key].length;
 
-	var at = Promise.resolve(true);
-	if (jcumines._workers[key].length > 0)
-		at = jcumines._workers[key][jcumines._workers[key].length - 1];
+    var at = Promise.resolve(true);
+    if (jcumines._workers[key].length > 0)
+        at = jcumines._workers[key][jcumines._workers[key].length - 1];
 
-	//Queue the operation behind the worker
-	var result = at.then(function (r) {
-			return Promise.resolve(input);
-		});
+    //Queue the operation behind the worker
+    var result = at.then(function(r) {
+        return Promise.resolve(input);
+    });
 
-	//Create a catching and self removing worker from our result.
-	var worker = result['catch'](function (err) {
-			//We just return the error (fulfills) because we dont want to kill it
-			return err;
-		}).then(function (r) {
-			//remove the worker from the array if it exists (it should
-			var removed = false;
-			for (var x = 0; x < jcumines._workers[key].length; x++) {
-				if (jcumines._workers[key][x] == worker) {
-					jcumines._workers[key].splice(0, 1);
-					x--;
-					removed = true;
-				}
-			}
-			if (!removed)
-				throw new Error('We couldnt self remove a promise in worker queue ' + key);
-		});
+    //Create a catching and self removing worker from our result.
+    var worker = result['catch'](function(err) {
+        //We just return the error (fulfills) because we dont want to kill it
+        return err;
+    }).then(function(r) {
+        //remove the worker from the array if it exists (it should
+        var removed = false;
+        for (var x = 0; x < jcumines._workers[key].length; x++) {
+            if (jcumines._workers[key][x] == worker) {
+                jcumines._workers[key].splice(0, 1);
+                x--;
+                removed = true;
+            }
+        }
+        if (!removed)
+            throw new Error('We couldnt self remove a promise in worker queue ' + key);
+    });
 
-	//add the worker to the end
-	jcumines._workers[key].push(worker);
+    //add the worker to the end
+    jcumines._workers[key].push(worker);
 
-	//Return the operation as well
-	return result;
+    //Return the operation as well
+    return result;
 };
 
 /**
 Deep copy of arrays and objects implementation. Only copies directly owned properties.
  */
-jcumines.deepCopy = function (obj) {
+jcumines.deepCopy = function(obj) {
     var result = obj;
-	if (jcumines.isObject(obj)) {
-		result = {};
-		for (var k in obj) {
-			if (obj.hasOwnProperty(k))
-				result[k] = jcumines.deepCopy(obj[k]);
-		}
-	} else if (jcumines.isArray(obj)) {
-		result = [];
-		for (var x = 0; x < obj.length; x++)
-			result.push(jcumines.deepCopy(obj[x]));
-	}
+    if (jcumines.isObject(obj)) {
+        result = {};
+        for (var k in obj) {
+            if (obj.hasOwnProperty(k))
+                result[k] = jcumines.deepCopy(obj[k]);
+        }
+    } else if (jcumines.isArray(obj)) {
+        result = [];
+        for (var x = 0; x < obj.length; x++)
+            result.push(jcumines.deepCopy(obj[x]));
+    }
     return result;
 };
 
 /**
 Async wait, returns a promise.
  */
-jcumines.wait = function (ms) {
-	try {
-		return new Promise(function (fulfill, reject) {
-			try {
-				var startedAt = Date.now();
-				setTimeout(function () {
-					fulfill(Date.now() - startedAt);
-				}, ms);
-			} catch (e) {
-				reject(e);
-			}
-		});
-	} catch (e) {
-		console.dir(e);
-		return Promise.reject(e);
-	}
+jcumines.wait = function(ms) {
+    try {
+        return new Promise(function(fulfill, reject) {
+            try {
+                var startedAt = Date.now();
+                setTimeout(function() {
+                    fulfill(Date.now() - startedAt);
+                }, ms);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    } catch (e) {
+        console.dir(e);
+        return Promise.reject(e);
+    }
 };
 
 /**
  * Returns true if the variable is an object.
  */
-jcumines.isObject = function (input) {
-	return typeof input == 'object' && input != null && !Array.isArray(input);
+jcumines.isObject = function(input) {
+    return typeof input == 'object' && input != null && !Array.isArray(input);
 };
 
 /**
 Returns true if the variable is an array.
  */
-jcumines.isArray = function (input) {
-	return input != null && Array.isArray(input);
+jcumines.isArray = function(input) {
+    return input != null && Array.isArray(input);
 };
 
 /**
@@ -119,24 +119,24 @@ Returns a promise, operates on every on every element of the array, using the ca
 
 Input must be an array, cb must be a callback taking one argument (the element), can return a promise.
  */
-jcumines.promiseAll = function (input, cb) {
-	try {
-		var copyInput = input.concat([]);
-		//done
-		if (copyInput.length === 0)
-			return Promise.resolve(true);
-		var working = copyInput[0];
-		copyInput.shift();
-		return Promise.resolve(true).then(function (r) {
-			return Promise.resolve(cb(working));
-		}).then(function (r) {
-			//call the next one.
-			return jcumines.promiseAll(copyInput, cb);
-		});
-	} catch (e) {
-		console.dir(e);
-		return new Promise.reject(e);
-	}
+jcumines.promiseAll = function(input, cb) {
+    try {
+        var copyInput = input.concat([]);
+        //done
+        if (copyInput.length === 0)
+            return Promise.resolve(true);
+        var working = copyInput[0];
+        copyInput.shift();
+        return Promise.resolve(true).then(function(r) {
+            return Promise.resolve(cb(working));
+        }).then(function(r) {
+            //call the next one.
+            return jcumines.promiseAll(copyInput, cb);
+        });
+    } catch (e) {
+        console.dir(e);
+        return new Promise.reject(e);
+    }
 };
 
 /**
@@ -145,9 +145,9 @@ Callback is required, will receive the line as param.
 
 Returns promise.
  */
-jcumines.forEachLine = function (s, cb) {
-	var split = s.match(/[^\r\n]+/g);
-	return jcumines.promiseAll(split, cb);
+jcumines.forEachLine = function(s, cb) {
+    var split = s.match(/[^\r\n]+/g);
+    return jcumines.promiseAll(split, cb);
 };
 
 /**
@@ -158,15 +158,15 @@ and args is an array.
 
 Throws an error if it is unable to execute.
  */
-jcumines.executeMethodOnObject = function (functionName, context, args) {
-	var namespaces = functionName.split(".");
-	var func = namespaces.pop();
-	for (var i = 0; i < namespaces.length; i++) {
-		context = context[namespaces[i]];
-	}
-	if (context[func] == null || context[func].apply == null)
-		throw new Error('Unable to execute ' + functionName + ' on the object provided.');
-	return context[func].apply(context, args);
+jcumines.executeMethodOnObject = function(functionName, context, args) {
+    var namespaces = functionName.split(".");
+    var func = namespaces.pop();
+    for (var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
+    }
+    if (context[func] == null || context[func].apply == null)
+        throw new Error('Unable to execute ' + functionName + ' on the object provided.');
+    return context[func].apply(context, args);
 };
 
 /**
@@ -174,15 +174,15 @@ Stringify an object, excluding seen objects.
 
 Basic way of serializing circular objects, such as results from mongodb.
  */
-jcumines.stringifyExcludeSeen = function (obj) {
-	var seen = [];
-	return JSON.stringify(obj, function (key, val) {
-		if (jcumines.isObject(val)) {
-			if (seen.indexOf(val) >= 0) {
-				return;
-			}
-			seen.push(val);
-		}
-		return val;
-	});
+jcumines.stringifyExcludeSeen = function(obj) {
+    var seen = [];
+    return JSON.stringify(obj, function(key, val) {
+        if (jcumines.isObject(val)) {
+            if (seen.indexOf(val) >= 0) {
+                return;
+            }
+            seen.push(val);
+        }
+        return val;
+    });
 };
